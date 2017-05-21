bbb48f3b0088e916891351d0a59daa1e71d8d587
Daniel Howlett <dan@dancras.co.uk>
2017-05-21 16:44:34 +0200

    Send single speech to text messages with every plugin button press

--- src/messengerSpeechToText.js
+++ src/messengerSpeechToText.js
@@ -3,7 +3,73 @@ const port = chrome.runtime.connect({
 });
 
 port.onMessage.addListener(function (msg) {
+
     console.log('msg received', msg);
+
+    const messageInputElement = findMessageInputElement();
+
+    messageInputElement.focus();
+
+    requireSpaceEvent(messageInputElement).then((spaceEvent) => {
+        window.setTimeout(() => {
+            messageInputElement.dispatchEvent(spaceEvent);
+            document.execCommand('insertText', false, msg);
+
+            window.setTimeout(() => {
+                const messageSendElement = findMessageSendElement();
+                console.log('clicking send', messageSendElement);
+                messageSendElement.click();
+            }, 0);
+        }, 0);
+    });
+
 });
+
+function findMessageInputElement () {
+
+    const matches = document.querySelectorAll('[aria-label="New message"] [contenteditable="true"]');
+
+    if (!matches) {
+        throw new Error('Unable to find message input element');
+    }
+
+    return matches[0];
+}
+
+function findMessageSendElement () {
+
+    const matches = document.querySelectorAll('[aria-label="New message"] a');
+
+    if (!matches) {
+        throw new Error('Unable to find message send element');
+    }
+
+    return matches[matches.length - 1];
+}
+
+function requireSpaceEvent (element) {
+
+    if (!element.messengerSpeechToTextSpaceEvent) {
+        element.messengerSpeechToTextSpaceEvent = new Promise(function (resolve) {
+
+            element.addEventListener('keypress', resolveOnSpaceKeypress, false);
+
+            function resolveOnSpaceKeypress (e) {
+
+                if (e.code === 'Space') {
+                    console.log('Space tracked');
+                    resolve(e);
+                }
+
+                element.removeEventListener('keypress', resolveOnSpaceKeypress);
+
+            }
+
+        });
+    }
+
+    return element.messengerSpeechToTextSpaceEvent;
+
+}
 
 console.log('messengerSpeechToTextPort created');
